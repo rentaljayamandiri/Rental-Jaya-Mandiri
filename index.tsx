b0af0c8a9aed4783f2ab7c03663fce359b615577
rentaljayamandiri <csrentaljayamandiri@gmail.com>
2026-02-13 21:40:51 +0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const rootElement = document.getElementById('root');
+
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error("Target container 'root' not found in index.html");
 }
 
 const root = ReactDOM.createRoot(rootElement);
